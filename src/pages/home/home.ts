0e1b69963ccda879b006aff1907be1ad2aa95e5b
Student <student@iMac-B202-04.local>
2018-10-24 16:39:20 +0200

    finish

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { TranslationProvider } from '../../providers/translation/translation';
 import { HistoryProvider } from '../../providers/history/history';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
 
 @Component({
   selector: 'page-home',
@@ -11,11 +12,13 @@ export class HomePage {
 
   public translationResult : string;
   public userInput:string;
+  private textToSpech:TextToSpeech;
 
   constructor(
     public navCtrl: NavController, 
     private translationProvider : TranslationProvider, 
-    private historyProvider:HistoryProvider) {
+    private historyProvider:HistoryProvider, private textToSpeach:TextToSpeech) {
+      this.textToSpech = textToSpeach;
   }
 
   btnTranslateClicked(userInput:string) : void {
@@ -26,7 +29,7 @@ export class HomePage {
         this.userInput = userInput;
         
         this.historyProvider.saveToStorage(userInput,this.translationResult);
-
+        this.textToSpech.speak(this.translationResult).then(() => console.log('Conplete'));
       }
 
     );
