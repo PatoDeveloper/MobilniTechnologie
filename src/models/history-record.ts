f334c031820a173c18e0af53b0ec9d2e49c8e271
Student <student@iMac-B202-04.local>
2018-10-24 15:31:23 +0200

    model class history record added

---
+++ src/models/history-record.ts
@@ -0,0 +1,12 @@
+export class HistoryRecord{
+    public from:string;
+    public to:string;
+
+    /**
+     *
+     */
+    constructor(from : string, to:string) {
+        this.from = from;
+        this.to = to;
+    }
+}
